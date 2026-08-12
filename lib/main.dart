d4f9a60e72062089b05126456d206f7e42f68ff6
ruby <112496980+ruby0v0@users.noreply.github.com>
2026-08-12 15:14:26 +0800

    feat: rebuild UI when state changes

--- lib/main.dart
+++ lib/main.dart
@@ -17,14 +17,7 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final viewModel = ArticleViewModel(ArticleModel());
-
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(title: const Text('Wikipedia Flutter')),
-        body: const Center(child: Text('Loading...')),
-      ),
-    );
+    return MaterialApp(home: ArticleView());
   }
 }
 
@@ -70,5 +63,107 @@ class ArticleViewModel extends ChangeNotifier {
 
     isLoading = false;
     notifyListeners();
+  }
+}
+
+class ArticleView extends StatefulWidget {
+  const ArticleView({super.key});
+
+  @override
+  State<StatefulWidget> createState() {
+    return _ArticleViewState();
+  }
+}
+
+class _ArticleViewState extends State<ArticleView> {
+  final ArticleViewModel viewModel = ArticleViewModel(ArticleModel());
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    viewModel.fetchArticle();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Wikipedia Flutter')),
+      body: ListenableBuilder(
+        listenable: viewModel,
+        builder: (context, _) {
+          return switch ((
+            viewModel.isLoading,
+            viewModel.summary,
+            viewModel.error,
+          )) {
+            (true, _, _) => const CircularProgressIndicator(),
+            (_, _, final Exception e) => Text('Error: $e'),
+            (_, final summary?, _) => ArticlePage(
+              summary: summary,
+              nextArticleCallback: viewModel.fetchArticle,
+            ),
+            _ => const Text('Something went wrong!'),
+          };
+        },
+      ),
+    );
+  }
+}
+
+class ArticlePage extends StatelessWidget {
+  const ArticlePage({
+    super.key,
+    required this.summary,
+    required this.nextArticleCallback,
+  });
+
+  final Summary summary;
+  final VoidCallback nextArticleCallback;
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          ArticleWidget(summary: summary),
+          ElevatedButton(
+            onPressed: nextArticleCallback,
+            child: Text('Next random article'),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class ArticleWidget extends StatelessWidget {
+  const ArticleWidget({super.key, required this.summary});
+
+  final Summary summary;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8),
+      child: Column(
+        spacing: 10,
+        children: [
+          if (summary.hasImage) Image.network(summary.originalImage!.source),
+          Text(
+            summary.titles.normalized,
+            overflow: TextOverflow.ellipsis,
+            style: Theme.of(context).textTheme.displaySmall,
+          ),
+          if (summary.description != null)
+            Text(
+              summary.description!,
+              overflow: TextOverflow.ellipsis,
+              style: Theme.of(context).textTheme.bodySmall,
+            ),
+          Text(summary.extract),
+        ],
+      ),
+    );
   }
 }
