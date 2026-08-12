65c321742a1b69ed3c219b688c512ee0fefb241d
ruby <112496980+ruby0v0@users.noreply.github.com>
2026-08-12 13:32:25 +0800

    set up project

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,10 @@
 import 'package:flutter/material.dart';
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart';
+
+import 'summary.dart';
 
 void main() {
   runApp(const MainApp());
@@ -9,11 +15,10 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+        appBar: AppBar(title: const Text('Wikipedia Flutter')),
+        body: const Center(child: Text('Loading...')),
       ),
     );
   }
